20aaffd5090ac29312f86bf7479020d35a7567be
yetttere <70298679+yetttere@users.noreply.github.com>
2020-08-27 23:39:19 -0400

    Add files via upload

---
+++ Exercise_3_2_do_four.py
@@ -0,0 +1,12 @@
+    #Hi, My name is Liz and this is a new skill for me.
+
+    #line 4 will define print_twice to ge the word spam twice
+def print_twice (bruce):
+    print (bruce,bruce)
+print_twice("spam")
+
+    #line 9 will define print four to get the word spam four times
+def print_four (bruce):
+    print(bruce, bruce, bruce, bruce)
+print_four("spam")
+
